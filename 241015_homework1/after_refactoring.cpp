bd0ac082cd23eb12d90c22b41cbba5a76f267e15
Gloria Khristich <glorse127@gmail.com>
2025-02-15 15:16:19 +0300

    HW1. Refactoring

---
+++ 241015_homework1/after_refactoring.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib> 
+#include <iostream>
+ 
+
+int main() {
+    const int size = 20;
+    int arr[size];      
+    for (int i = 0; i < size; i++) {
+        arr[i] = std::rand() % 20; 
+    }
+
+    std::cout << "Cгенерированные случайные числа:" << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+
+    std::cout << "Перевернутые числа:" << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
