42551feb6c62156b3400d883ba0781462e6d2965
Gloria Khristich <glorse127@gmail.com>
2025-03-15 13:34:19 +0300

    Refactor after creating cmake project

--- 250215_cmake_prj/build_run.bat
+++ 250215_cmake_prj/build_run.bat
@@ -5,7 +5,7 @@ set BUILD_TYPE=Ninja
 set BUILD_SUFFIX=ninja
 
 set BUILD_FOLDER=build_%BUILD_SUFFIX%
-set SOURCE_FOLDER=project
+set SOURCE_FOLDER=projects
 
 if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
 
